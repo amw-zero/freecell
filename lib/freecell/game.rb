e1378141ace83fca5e30d382dde943132fcf625e
Alex Weisberger <alex.weisberger@viewthespace.com>
2018-07-05 08:44:36 -0400

    [Refactor] - annotated responsibilities
    Here we see that even a seemingly small code block can contain a number of
    responsibility smells. The Game object handles user input, manages the state of
    the game (right now just the cascades), and checks for the legality of moves.
    This leads to methods that have several different responsibilities at once.

--- lib/freecell/game.rb
+++ lib/freecell/game.rb
@@ -3,24 +3,37 @@ module Freecell
   class Game
     attr_reader :cascades
 
+    # Says it's a game, but also handles user input
     def initialize(cascades: [])
+      # Game state
       @cascades = cascades
+
+      # Input handling
       @move_parts = []
     end
 
+    # Says it's handling Input keys, but it also manipulates Game state
     def handle_key(key)
+      # Input handling
       @move_parts << key_to_cascade_idx(key)
+
+      # Game state
       @cascades[1] << @cascades[0].pop if legal_move?
     end
 
+    # Says it's checking for Moves legality, but also checks Input
     def legal_move?
+      # Input handling
       return false if @move_parts.length < 2
-
       src_cascade_idx, dest_cascade_idx = @move_parts
+
+      # Move legality
       last_cascade_card(src_cascade_idx) < last_cascade_card(dest_cascade_idx)
     end
 
+    # One responsibility: Map Input keys to Game state indices
     def key_to_cascade_idx(key)
+      # Input handling
       case key
       when 'a'
         0
@@ -29,7 +42,9 @@ module Freecell
       end
     end
 
+    # One responsibility: syntactic sugar for accessing Game state
     def last_cascade_card(cascade_idx)
+      # Game state
       @cascades[cascade_idx].last
     end
   end
