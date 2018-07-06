14d333aa0e07c879f587b5ad1de5b903af541619
Alex Weisberger <alex.weisberger@viewthespace.com>
2018-07-06 08:42:18 -0400

    Attempt to implement move legality checking before moving cards
    Cards shouldn't move between cascades when they are not legal moves. Add a
    failing spec for this behavior, and provide an attempted implementation. Several
    responsibilities are already being mixed in the Game object, so skip the test in
    favor of refactoring.

--- lib/freecell/game.rb
+++ lib/freecell/game.rb
@@ -5,10 +5,32 @@ module Freecell
 
     def initialize(cascades: [])
       @cascades = cascades
+      @move_parts = []
     end
 
     def handle_key(key)
-      @cascades[1] << @cascades[0].pop if key == 'b'
+      @move_parts << key_to_cascade_idx(key)
+      @cascades[1] << @cascades[0].pop if legal_move?
+    end
+
+    def legal_move?
+      return false if @move_parts.length < 2
+
+      src_cascade_idx, dest_cascade_idx = @move_parts
+      last_cascade_card(src_cascade_idx) < last_cascade_card(dest_cascade_idx)
+    end
+
+    def key_to_cascade_idx(key)
+      case key
+      when 'a'
+        0
+      when 'b'
+        1
+      end
+    end
+
+    def last_cascade_card(cascade_idx)
+      @cascades[cascade_idx].last
     end
   end
 end
--- spec/freecell_spec.rb
+++ spec/freecell_spec.rb
@@ -2,15 +2,15 @@ RSpec.describe Freecell do
   let(:game) { Freecell::Game.new(cascades: cascades) }
 
   describe 'cascade moves' do
+    subject(:cascade_cards) do
+      [game.cascades[0].last, game.cascades[1].last]
+    end
+
     let(:five_of_spades) { 5 }
     let(:four_of_diamonds) { 4 }
     let(:cascades) { [cascade_one, cascade_two] }
 
     context 'when the move is legal' do
-      subject(:cascade_cards) do
-        [game.cascades[0].last, game.cascades[1].last]
-      end
-
       let(:cascade_one) { [four_of_diamonds] }
       let(:cascade_two) { [five_of_spades] }
 
@@ -21,6 +21,20 @@ RSpec.describe Freecell do
 
       it 'moves cards between cascades' do
         expect(cascade_cards).to eq([nil, four_of_diamonds])
+      end
+    end
+
+    skip 'when the move is not legal' do
+      let(:cascade_one) { [four_of_diamonds] }
+      let(:cascade_two) { [five_of_spades] }
+
+      before do
+        game.handle_key('b')
+        game.handle_key('a')
+      end
+
+      it 'does not move cards between cascades' do
+        expect(cascade_cards).to eq([four_of_diamonds, five_of_spades])
       end
     end
   end
