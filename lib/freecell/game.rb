45b14cfb89907a9b46bcaaa0050ab1293d72d12c
Alex Weisberger <alex.weisberger@viewthespace.com>
2018-07-11 22:35:36 -0400

    [Refactor] - Annotated refactor rationale
    Here we create a failing test for moving cards from cascades to free cells. We
    can see that the current code is very specific for moves between cascades. We'll
    need to refactor to support adding additional move types.

--- lib/freecell/game.rb
+++ lib/freecell/game.rb
@@ -3,8 +3,9 @@ module Freecell
   class Game
     attr_reader :cascades
 
-    def initialize(cascades: [])
+    def initialize(cascades: [], free_cells: [])
       @cascades = cascades
+      @free_cells = free_cells
       @input_handler = InputHandler.new
     end
 
@@ -14,12 +15,20 @@ module Freecell
 
     def handle_key(key)
       @input_handler.handle_key(key)
+
+      # Different moves and their corresponding legality checks will
+      # need to be supported here
+      #
+      # Need to pivot on legality check
       return unless @input_handler.move_complete &&
                     legal_move?(@input_handler.current_move)
 
+      # Need to pivot on arguments to move
       src_idx, dest_idx = @input_handler
                           .current_move
                           .values_at(:src_idx, :dest_idx)
+
+      # Need to pivot on state to manipulate
       @cascades[dest_idx] << @cascades[src_idx].pop
     end
 
