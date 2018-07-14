5524afb1de69c7eb4221cb96bdceee7b231a09d2
Alex Weisberger <alex.weisberger@viewthespace.com>
2018-07-13 21:56:53 -0400

    Render free cells in the UI

--- lib/freecell/ncurses_ui.rb
+++ lib/freecell/ncurses_ui.rb
@@ -3,6 +3,8 @@ require 'curses'
 module Freecell
   # Commandline UI
   class NcursesUI
+    CASCADE_MARGIN = 3
+
     def initialize
       Curses.init_screen
       Curses.cbreak
@@ -21,18 +23,42 @@ module Freecell
     def render(game)
       @y_pos = 0
       Curses.clear
+      render_top_area(game)
       render_cascades(game)
       render_cascade_letters
     end
 
+    def advance_y(by:, x_pos: 0)
+      @y_pos += by
+      Curses.setpos(@y_pos, x_pos)
+    end
+
+    def render_top_area(game)
+      Curses.addstr('space')
+      advance_y(by: 1)
+      render_free_cells(game)
+      advance_y(by: 1, x_pos: 2)
+      game.free_cells.length.times { |i| Curses.addstr(free_cell_letter(i)) }
+    end
+
+    def render_free_cells(game)
+      game.free_cells.each { |card| Curses.addstr("[#{card}]") }
+      (4 - game.free_cells.length).times { Curses.addstr("[#{NullCard.new}]") }
+    end
+
+    def free_cell_letter(index)
+      "#{%w[w x y z][index]}    "
+    end
+
     def render_cascades(game)
+      advance_y(by: 2, x_pos: CASCADE_MARGIN)
       CardGrid.new(game.cascades)
               .row_representation
               .each(&method(:print_card_row))
     end
 
     def render_cascade_letters
-      Curses.setpos(@y_pos + 1, 0)
+      advance_y(by: 1, x_pos: CASCADE_MARGIN)
       %w[a b c d e f g h].each do |letter|
         Curses.addstr(" #{letter}   ")
       end
@@ -42,8 +68,7 @@ module Freecell
       row.each do |card|
         Curses.addstr("#{card}  ")
       end
-      @y_pos += 1
-      Curses.setpos(@y_pos, 0)
+      advance_y(by: 1, x_pos: CASCADE_MARGIN)
     end
   end
 end
